aff557563c37bfe5e1ae20f9e618851757bc12f8
claudio996 <claudiantonio.retamal@gmail.com>
2023-01-29 00:05:30 -0300

    GLOBAL STYLE

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function GifExpertApp() {
     return (
         <>
+            {/*TITLE*/}
             <h1>Gif</h1>
+            {/*INPUT */}
+            {/* GIF STADE */}
+            {/*GIF ITEM */}
         </>
     )
 }
