cb09e0e7bcc7f72f0ece2c241ef7f86cb7e92731
claudio996 <claudiantonio.retamal@gmail.com>
2023-01-29 00:59:15 -0300

    ADD-CATEGORY WITH SPREET OPERATOR IN ARRAY

---
+++ readme.md
@@ -0,0 +1,11 @@
+
+# CREATE FIRST COMPONENT
+### render to root component
+#### import style
+
+# ELABORATE COMPONENT GIF-EXPERT
+# CREATE ESTATE 
+# ADD-CATEGORIES
+
+
+
--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
-export default function GifExpertApp() {
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['ONE PUCH', 'DRAGON BALL'])
+
+    const addCategory = () => {
+        setCategories([...categories, 'CABALLEROS DEL ZODIACO']); //with spreet operator create copi of array categories  en include new element
+    }
+
     return (
         <>
             {/*TITLE*/}
             <h1>Gif</h1>
             {/*INPUT */}
-            {/* GIF STADE */}
+            <button onClick={addCategory}>AddCat</button>
+            {/* GIF LISTS */}
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+                <li></li>
+            </ol>
             {/*GIF ITEM */}
         </>
     )
